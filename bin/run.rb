7ef6718fb2340c45fb9d250f7ae2c3694bf363ae
21shield <21netaly@gmail.com>
2020-07-08 09:48:47 -0400

    added the exit button the password button doesnt work yet

--- bin/run.rb
+++ bin/run.rb
@@ -6,6 +6,7 @@ main_interface = Main.new()
 Main.welcome
 # this below will call the remaining methods
 user = main_interface.login_register_prompt
+main_interface.user = user
 
 #  Main.welcome
  main_interface.users_interface(user)
