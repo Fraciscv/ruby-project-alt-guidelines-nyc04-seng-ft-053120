9aeb9c04840daf480bcff9be914bd81f22316de0
21shield <21netaly@gmail.com>
2020-07-09 10:59:32 -0400

    added new methods for comment availibility

--- lib/apps/models/joke.rb
+++ lib/apps/models/joke.rb
@@ -57,22 +57,40 @@ class Joke < ActiveRecord::Base
         end
     end
     def display_joke(user_instance)
-        Main.bmo
+        
         joke_table = TTY::Table.new ["Your Joke is :"],[[self.content]]
 
         puts joke_table.render(:unicode, alignments: [:center, :center], padding: [1,1,0,1])
+      
         self.users_next_joke(user_instance)
+        Main.bmo
     end
 
     def users_next_joke(user_instance)
         users_input = self.class.prompt.select("What would you like to do next?", ["Give a comment!!", "Take me to Jokes main"])
         if users_input == "Give a comment!!"
             # make a comment from user
+            self.add_a_comment(user_instance)
         else users_input == "Take me to Jokes main"
             # go back to jokes main interface
             Main.bmo
             
             Joke.joke_interface(user_instance)
         end
+    end
+
+    def add_a_comment(user_instance)
+        users_input = self.class.prompt.ask("What are your thoughts?")
+        new_comment = Comment.create(content:users_input, joke_id: self.id, user_id: user_instance.id)
+        binding.pry
+        self.display(new_comment)
+    end
+
+    def display(comment)
+        
+        comment_table = TTY::Table.new ["Comment :", "user"],[
+        [comment.content, comment.user_id]]
+        puts comment_table.render(:unicode, alignments: [:center, :center], padding: [1,1,0,1])
+        
     end
 end
